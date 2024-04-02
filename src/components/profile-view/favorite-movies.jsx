45b2b129621d492a8f62a841e73e58217a02c65b
rweathers89 <146603179+rweathers89@users.noreply.github.com>
2024-04-01 18:17:38 -0700

    typo

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Row, Col, Figure, Button } from "react-bootstrap";
 import { MovieCard } from "../movie-card/movie-card";
-import "./profil-view.scss";
+import "./profile-view.scss";
 import { Link } from "react-router-dom";
 
 export const FavouriteMovies = ({ user, favoriteMovies }) => {
