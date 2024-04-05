0fce38f0a2b484f97f798f21694b90ffa8426547
rweathers89 <146603179+rweathers89@users.noreply.github.com>
2024-04-04 18:36:12 -0700

    updated navbar with Profile link

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -8,61 +8,41 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
     const dispatch = useDispatch();
 
     return (
-        <>
-            <Row className="navBar">
-                <Navbar bg="light" expand="lg">
-                    <Container>
-                        <Navbar.Brand as={Link} to="/">
-                            MyMovieMix
-                        </Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="me-auto">
-                                {!user && (
-                                    <>
-                                        <Nav.Link className="navLink" as={Link} to="/login">
-                                            Login
-                                        </Nav.Link>
-                                        <Nav.Link className="navLink" as={Link} to="/signup">
-                                            Signup
-                                        </Nav.Link>
-                                    </>
-                                )}
-                                {!user && (
-                                    <>
-                                        <Nav.Link className="navLink" as={Link} to="/">
-                                            Home
-                                        </Nav.Link>
-                                        <Nav.Link className="navLink" as={Link} to="/profile">
-                                            Profile
-                                        </Nav.Link>
-                                        <Nav.Link className="navLink" onClick={() => dispatch(setUser(null))} >
-                                            Logout
-                                        </Nav.Link>
-                                    </>
-                                )}
-                            </Nav>
-                            <Routes>
-                                <Route
-                                    path="/"
-                                    element={
-                                        <Form inline="true">
-                                            <Row>
-                                                <Col xs="auto">
-                                                    <SearchBar
-                                                        handleSearch={handleSearch}
-                                                        query={query}
-                                                        movies={movies} />
-                                                </Col>
-                                            </Row>
-                                        </Form>
-                                    }
-                                />
-                            </Routes>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-            </Row>
-        </>
+
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand as={Link} to="/">
+                    My Movie Mix
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        {!user && (
+                            <>
+                                <Nav.Link className="navLink" as={Link} to="/login">
+                                    Login
+                                </Nav.Link>
+                                <Nav.Link className="navLink" as={Link} to="/signup">
+                                    Signup
+                                </Nav.Link>
+                            </>
+                        )}
+                        {!user && (
+                            <>
+                                <Nav.Link className="navLink" as={Link} to="/">
+                                    Home
+                                </Nav.Link>
+                                <Nav.Link className="navLink" as={Link} to="/profile">
+                                    Profile
+                                </Nav.Link>
+                                <Nav.Link className="navLink" onClick={() => dispatch(setUser(null))} >
+                                    Logout
+                                </Nav.Link>
+                            </>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
